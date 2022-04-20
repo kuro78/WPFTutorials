dc56f54ae0fe35433ffb0124e063b73ea1597bf4
kuro <kuro78@gmail.com>
2022-04-20 22:55:34 +0900

    INotifyPropertyChanged

---
+++ INPC/INPC/PersonModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace INPC
+{
+    internal class PersonModel : ObservableObject
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; OnPropertyChanged(); }
+        }
+
+        public PersonModel()
+        {
+            Task.Run(() =>
+            {
+                while(true)
+                {
+                    Random r = new();
+                    Name = r.Next(1, 1000).ToString();
+                    Debug.WriteLine($"Name: {Name}");
+                    Thread.Sleep(500);
+                }
+            });
+        }
+
+    }
+}
